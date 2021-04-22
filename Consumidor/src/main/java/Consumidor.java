45833633d5d1e29b540f523faffa69ee6f40d1a0
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2021-04-22 20:44:57 -0300

    Commit inicial

---
+++ Consumidor/src/main/java/Consumidor.java
@@ -0,0 +1,6 @@
+
+public class Consumidor {
+    public static void main(String[] args) {
+        System.out.println("Consumidor");
+    }
+}
---
+++ Produtor/src/main/java/Produtor.java
@@ -0,0 +1,6 @@
+
+public class Produtor {
+    public static void main(String[] args) {
+            System.out.println("Produtor");
+    }
+}
